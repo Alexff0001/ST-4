10a34418e62836221a9360dfd212328971e3f059
Alexff0001 <alexbadanov1@gmail.com>
2026-04-26 21:17:27 +0300

    initial

---
+++ BugPro/Program.cs
@@ -0,0 +1,246 @@
+﻿using Stateless;
+using System;
+
+namespace BugPro
+{
+    public class Bug
+    {
+        public enum State
+        {
+            New,                // НОВЫЙ ДЕФЕКТ
+            Analysis,           // РАЗБОР ДЕФЕКТОВ
+            Fixing,             // ИСПРАВЛЕНИЕ
+            NotABug,            // НЕ ДЕФЕКТ
+            WontFix,            // НЕ ИСПРАВЛЯТЬ
+            Duplicate,          // ДУБЛИРОВАНИЕ
+            NotReproducible,    // НЕ ВОСПРОИЗВОДИМО
+            Closed,             // ЗАКРЫТИЕ
+            Return,             // ВОЗВРАТ
+            Reopened,           // ПЕРЕОТКРЫТИЕ
+            NoTime,             // НЕТ ВРЕМЕНИ СЕЙЧАС
+            SeparateSolution,   // НУЖНО ОТДЕЛЬНОЕ РЕШЕНИЕ
+            OtherProduct,       // ПРОБЛЕМА ДРУГОГО ПРОДУКТА
+            MoreInfo            // НУЖНО БОЛЬШЕ ИНФОРМАЦИИ
+        }
+
+        public enum Trigger
+        {
+            StartAnalysis,      // Начать разбор дефектов
+            StartFixing,        // Начать исправление
+            MarkNotABug,        // Пометить как "не дефект"
+            MarkWontFix,        // Пометить как "не исправлять"
+            MarkDuplicate,      // Пометить как дублирование
+            MarkNotReproducible,// Пометить как "не воспроизводимо"
+            ProblemSolved,      // Проблема решена (ДА)
+            ProblemNotSolved,   // Проблема не решена (НЕТ)
+            Close,              // Закрыть
+            ReturnToAnalysis,   // Вернуть на разбор
+            Reopen,             // Переоткрыть
+            NoTimeNow,          // Нет времени сейчас
+            NeedSeparateSolution,// Нужно отдельное решение
+            ProblemOtherProduct,// Проблема другого продукта
+            NeedMoreInfo,       // Нужно больше информации
+            ConfirmOK,          // Подтвердить OK
+            ConfirmNotOK        // Подтвердить не OK
+        }
+
+        private readonly StateMachine<State, Trigger> _machine;
+
+        public Bug()
+        {
+            _machine = new StateMachine<State, Trigger>(State.New);
+
+            _machine.Configure(State.New)
+                .Permit(Trigger.StartAnalysis, State.Analysis);
+
+            _machine.Configure(State.Analysis)
+                .Permit(Trigger.StartFixing, State.Fixing)
+                .Permit(Trigger.MarkNotABug, State.NotABug)
+                .Permit(Trigger.MarkWontFix, State.WontFix)
+                .Permit(Trigger.MarkDuplicate, State.Duplicate);
+
+            _machine.Configure(State.Fixing)
+                .Permit(Trigger.ProblemSolved, State.Closed)
+                .Permit(Trigger.ProblemNotSolved, State.Return)
+                .Permit(Trigger.MarkNotReproducible, State.NotReproducible)
+                .Permit(Trigger.NoTimeNow, State.NoTime)
+                .Permit(Trigger.NeedSeparateSolution, State.SeparateSolution)
+                .Permit(Trigger.ProblemOtherProduct, State.OtherProduct)
+                .Permit(Trigger.NeedMoreInfo, State.MoreInfo);
+
+            _machine.Configure(State.NotReproducible)
+                .Permit(Trigger.ConfirmOK, State.Closed)
+                .Permit(Trigger.ConfirmNotOK, State.Return);
+
+            _machine.Configure(State.NotABug)
+                .Permit(Trigger.ReturnToAnalysis, State.Analysis);
+
+            _machine.Configure(State.WontFix)
+                .Permit(Trigger.ReturnToAnalysis, State.Analysis);
+
+            _machine.Configure(State.Duplicate)
+                .Permit(Trigger.ReturnToAnalysis, State.Analysis);
+
+            _machine.Configure(State.Return)
+                .Permit(Trigger.StartAnalysis, State.Analysis);
+
+            _machine.Configure(State.NoTime)
+                .Permit(Trigger.ReturnToAnalysis, State.Analysis);
+
+            _machine.Configure(State.SeparateSolution)
+                .Permit(Trigger.ReturnToAnalysis, State.Analysis);
+
+            _machine.Configure(State.OtherProduct)
+                .Permit(Trigger.ReturnToAnalysis, State.Analysis);
+
+            _machine.Configure(State.MoreInfo)
+                .Permit(Trigger.ReturnToAnalysis, State.Analysis);
+
+            _machine.Configure(State.Closed)
+                .Permit(Trigger.Reopen, State.Reopened);
+
+            _machine.Configure(State.Reopened)
+                .Permit(Trigger.StartAnalysis, State.Analysis);
+        }
+
+        public State CurrentState => _machine.State;
+
+        public void StartAnalysis()
+        {
+            _machine.Fire(Trigger.StartAnalysis);
+            Console.WriteLine($"Начат разбор дефектов. Состояние: {_machine.State}");
+        }
+
+        public void StartFixing()
+        {
+            _machine.Fire(Trigger.StartFixing);
+            Console.WriteLine($"Начато исправление. Состояние: {_machine.State}");
+        }
+
+        public void MarkNotABug()
+        {
+            _machine.Fire(Trigger.MarkNotABug);
+            Console.WriteLine($"Помечено как 'не дефект'. Состояние: {_machine.State}");
+        }
+
+        public void MarkWontFix()
+        {
+            _machine.Fire(Trigger.MarkWontFix);
+            Console.WriteLine($"Помечено как 'не исправлять'. Состояние: {_machine.State}");
+        }
+
+        public void MarkDuplicate()
+        {
+            _machine.Fire(Trigger.MarkDuplicate);
+            Console.WriteLine($"Помечено как дублирование. Состояние: {_machine.State}");
+        }
+
+        public void MarkNotReproducible()
+        {
+            _machine.Fire(Trigger.MarkNotReproducible);
+            Console.WriteLine($"Помечено как 'не воспроизводимо'. Состояние: {_machine.State}");
+        }
+
+        public void ProblemSolved()
+        {
+            _machine.Fire(Trigger.ProblemSolved);
+            Console.WriteLine($"Проблема решена. Состояние: {_machine.State}");
+        }
+
+        public void ProblemNotSolved()
+        {
+            _machine.Fire(Trigger.ProblemNotSolved);
+            Console.WriteLine($"Проблема не решена. Состояние: {_machine.State}");
+        }
+
+        public void Close()
+        {
+            _machine.Fire(Trigger.Close);
+            Console.WriteLine($"Дефект закрыт. Состояние: {_machine.State}");
+        }
+
+        public void ReturnToAnalysis()
+        {
+            _machine.Fire(Trigger.ReturnToAnalysis);
+            Console.WriteLine($"Возврат на разбор. Состояние: {_machine.State}");
+        }
+
+        public void Reopen()
+        {
+            _machine.Fire(Trigger.Reopen);
+            Console.WriteLine($"Дефект переоткрыт. Состояние: {_machine.State}");
+        }
+
+        public void NoTimeNow()
+        {
+            _machine.Fire(Trigger.NoTimeNow);
+            Console.WriteLine($"Нет времени сейчас. Состояние: {_machine.State}");
+        }
+
+        public void NeedSeparateSolution()
+        {
+            _machine.Fire(Trigger.NeedSeparateSolution);
+            Console.WriteLine($"Нужно отдельное решение. Состояние: {_machine.State}");
+        }
+
+        public void ProblemOtherProduct()
+        {
+            _machine.Fire(Trigger.ProblemOtherProduct);
+            Console.WriteLine($"Проблема другого продукта. Состояние: {_machine.State}");
+        }
+
+        public void NeedMoreInfo()
+        {
+            _machine.Fire(Trigger.NeedMoreInfo);
+            Console.WriteLine($"Нужно больше информации. Состояние: {_machine.State}");
+        }
+
+        public void ConfirmOK()
+        {
+            _machine.Fire(Trigger.ConfirmOK);
+            Console.WriteLine($"Подтверждено OK. Состояние: {_machine.State}");
+        }
+
+        public void ConfirmNotOK()
+        {
+            _machine.Fire(Trigger.ConfirmNotOK);
+            Console.WriteLine($"Подтверждено не OK. Состояние: {_machine.State}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Демонстрация WorkFlow работы с багом ===\n");
+
+            var bug = new Bug();
+            Console.WriteLine($"Начальное состояние: {bug.CurrentState}\n");
+
+            bug.StartAnalysis();
+            bug.StartFixing();
+            bug.ProblemSolved();
+            Console.WriteLine($"\nФинальное состояние: {bug.CurrentState}\n");
+
+            Console.WriteLine("--- Тест переоткрытия ---");
+            var bug2 = new Bug();
+            bug2.StartAnalysis();
+            bug2.StartFixing();
+            bug2.ProblemSolved();
+            bug2.Reopen();
+            bug2.StartAnalysis();
+            Console.WriteLine($"Финальное состояние: {bug2.CurrentState}\n");
+
+            Console.WriteLine("--- Тест невоспроизводимости ---");
+            var bug3 = new Bug();
+            bug3.StartAnalysis();
+            bug3.StartFixing();
+            bug3.MarkNotReproducible();
+            bug3.ConfirmNotOK();
+            bug3.StartAnalysis();
+            Console.WriteLine($"Финальное состояние: {bug3.CurrentState}\n");
+
+            Console.WriteLine("=== Демонстрация завершена ===");
+        }
+    }
+}
---
+++ BugTests/UnitTest1.cs
@@ -0,0 +1,234 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BugPro;
+using System;
+
+namespace BugTests
+{
+    [TestClass]
+    public class BugStateMachineTests
+    {
+        private Bug _bug = null!;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _bug = new Bug();
+        }
+
+        [TestMethod]
+        public void InitialState_ShouldBe_New()
+        {
+            Assert.AreEqual(Bug.State.New, _bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void StartAnalysis_FromNew_ShouldMoveToAnalysis()
+        {
+            _bug.StartAnalysis();
+            Assert.AreEqual(Bug.State.Analysis, _bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void StartFixing_FromAnalysis_ShouldMoveToFixing()
+        {
+            _bug.StartAnalysis();
+            _bug.StartFixing();
+            Assert.AreEqual(Bug.State.Fixing, _bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void MarkNotABug_FromAnalysis_ShouldMoveToNotABug()
+        {
+            _bug.StartAnalysis();
+            _bug.MarkNotABug();
+            Assert.AreEqual(Bug.State.NotABug, _bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void MarkWontFix_FromAnalysis_ShouldMoveToWontFix()
+        {
+            _bug.StartAnalysis();
+            _bug.MarkWontFix();
+            Assert.AreEqual(Bug.State.WontFix, _bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void MarkDuplicate_FromAnalysis_ShouldMoveToDuplicate()
+        {
+            _bug.StartAnalysis();
+            _bug.MarkDuplicate();
+            Assert.AreEqual(Bug.State.Duplicate, _bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void ProblemSolved_FromFixing_ShouldMoveToClosed()
+        {
+            _bug.StartAnalysis();
+            _bug.StartFixing();
+            _bug.ProblemSolved();
+            Assert.AreEqual(Bug.State.Closed, _bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void ProblemNotSolved_FromFixing_ShouldMoveToReturn()
+        {
+            _bug.StartAnalysis();
+            _bug.StartFixing();
+            _bug.ProblemNotSolved();
+            Assert.AreEqual(Bug.State.Return, _bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void MarkNotReproducible_FromFixing_ShouldMoveToNotReproducible()
+        {
+            _bug.StartAnalysis();
+            _bug.StartFixing();
+            _bug.MarkNotReproducible();
+            Assert.AreEqual(Bug.State.NotReproducible, _bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void ConfirmOK_FromNotReproducible_ShouldMoveToClosed()
+        {
+            _bug.StartAnalysis();
+            _bug.StartFixing();
+            _bug.MarkNotReproducible();
+            _bug.ConfirmOK();
+            Assert.AreEqual(Bug.State.Closed, _bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void ConfirmNotOK_FromNotReproducible_ShouldMoveToReturn()
+        {
+            _bug.StartAnalysis();
+            _bug.StartFixing();
+            _bug.MarkNotReproducible();
+            _bug.ConfirmNotOK();
+            Assert.AreEqual(Bug.State.Return, _bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void NoTimeNow_FromFixing_ShouldMoveToNoTime()
+        {
+            _bug.StartAnalysis();
+            _bug.StartFixing();
+            _bug.NoTimeNow();
+            Assert.AreEqual(Bug.State.NoTime, _bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void NeedSeparateSolution_FromFixing_ShouldMoveToSeparateSolution()
+        {
+            _bug.StartAnalysis();
+            _bug.StartFixing();
+            _bug.NeedSeparateSolution();
+            Assert.AreEqual(Bug.State.SeparateSolution, _bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void ProblemOtherProduct_FromFixing_ShouldMoveToOtherProduct()
+        {
+            _bug.StartAnalysis();
+            _bug.StartFixing();
+            _bug.ProblemOtherProduct();
+            Assert.AreEqual(Bug.State.OtherProduct, _bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void NeedMoreInfo_FromFixing_ShouldMoveToMoreInfo()
+        {
+            _bug.StartAnalysis();
+            _bug.StartFixing();
+            _bug.NeedMoreInfo();
+            Assert.AreEqual(Bug.State.MoreInfo, _bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void Reopen_FromClosed_ShouldMoveToReopened()
+        {
+            _bug.StartAnalysis();
+            _bug.StartFixing();
+            _bug.ProblemSolved();
+            _bug.Reopen();
+            Assert.AreEqual(Bug.State.Reopened, _bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void StartAnalysis_FromReopened_ShouldMoveToAnalysis()
+        {
+            _bug.StartAnalysis();
+            _bug.StartFixing();
+            _bug.ProblemSolved();
+            _bug.Reopen();
+            _bug.StartAnalysis();
+            Assert.AreEqual(Bug.State.Analysis, _bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void ReturnToAnalysis_FromNotABug_ShouldMoveToAnalysis()
+        {
+            _bug.StartAnalysis();
+            _bug.MarkNotABug();
+            _bug.ReturnToAnalysis();
+            Assert.AreEqual(Bug.State.Analysis, _bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void ReturnToAnalysis_FromNoTime_ShouldMoveToAnalysis()
+        {
+            _bug.StartAnalysis();
+            _bug.StartFixing();
+            _bug.NoTimeNow();
+            _bug.ReturnToAnalysis();
+            Assert.AreEqual(Bug.State.Analysis, _bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void StartAnalysis_FromReturn_ShouldMoveToAnalysis()
+        {
+            _bug.StartAnalysis();
+            _bug.StartFixing();
+            _bug.ProblemNotSolved();
+            _bug.StartAnalysis();
+            Assert.AreEqual(Bug.State.Analysis, _bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void StartFixing_FromNew_ShouldThrowException()
+        {
+            Assert.Throws<InvalidOperationException>(() => _bug.StartFixing());
+        }
+
+        [TestMethod]
+        public void Reopen_FromNew_ShouldThrowException()
+        {
+            Assert.Throws<InvalidOperationException>(() => _bug.Reopen());
+        }
+
+        [TestMethod]
+        public void ProblemSolved_FromAnalysis_ShouldThrowException()
+        {
+            _bug.StartAnalysis();
+            Assert.Throws<InvalidOperationException>(() => _bug.ProblemSolved());
+        }
+
+        [TestMethod]
+        public void ConfirmOK_FromNew_ShouldThrowException()
+        {
+            Assert.Throws<InvalidOperationException>(() => _bug.ConfirmOK());
+        }
+
+        [TestMethod]
+        public void FullCycle_WithReopen_ShouldWork()
+        {
+            _bug.StartAnalysis();
+            _bug.StartFixing();
+            _bug.ProblemSolved();
+            _bug.Reopen();
+            _bug.StartAnalysis();
+            _bug.StartFixing();
+            _bug.ProblemSolved();
+            Assert.AreEqual(Bug.State.Closed, _bug.CurrentState);
+        }
+    }
+}
